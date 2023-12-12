ee75196a062dae4a60ab952e1333523e6b854cfe
SulienP <112190974+SulienP@users.noreply.github.com>
2023-12-12 13:52:43 +0100

    :construction: | upgrade api road

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\API\UserController;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -17,6 +19,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::apiResource("users", UserController::class);
+
 
 Route::get('/greeting', function () {
     return 'Hello World';
